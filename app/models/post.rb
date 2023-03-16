2d18cd0b6725ec9bb795b26a3d7f04ed58ed4d5f
Tracy Mumbi <tracychiuri@gmail.com>
2023-03-17 00:17:02 +0300

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validates :content, length: {in: 100..500}
 end
